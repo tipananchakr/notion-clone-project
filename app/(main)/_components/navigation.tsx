95dbd51a5ec1f13714a03a4f29abb8748e9f3422
tipananchakr <tipanan.chak@gmail.com>
2026-05-22 21:18:44 +0700

    ➕ Add method get for Documents

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,14 +4,18 @@ import { usePathname } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import { UserItem } from "./user-item"
+import { useQuery } from "convex/react"
+import { api } from "@/convex/_generated/api"
 
 export const Navigation = () => {
   const pathname = usePathname()
   const isMobile = useMediaQuery("(max-width: 768px)")
+  const documents = useQuery(api.documents.get)
 
   const isResizingRef = useRef(false)
   const sidebarRef = useRef<ElementRef<"aside">>(null)
   const navbarRef = useRef<ElementRef<"div">>(null)
+  
   const [isResetting, setIsResetting] = useState(false)
   const [isCollapsed, setIsCollapsed] = useState(false)
 
@@ -114,7 +118,11 @@ export const Navigation = () => {
         </div>
 
         <div className="mt-4">
-          <p>Document</p>
+          {documents?.map((document) => (
+            <p key={document._id}>
+              {document.title}
+            </p>
+          ))}
         </div>
 
         <div 
--- convex/documents.ts
+++ convex/documents.ts
@@ -27,3 +27,18 @@ export const create = mutation({
     return document
   }
 })
+
+export const get = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated")
+    }
+
+    const documents = await ctx.db.query("documents").collect()
+
+    return documents
+
+  }
+})
