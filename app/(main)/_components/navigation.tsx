5707e8bbe635882582813a0c32fc02f9ccc88f73
tipananchakr <tipanan.chak@gmail.com>
2026-05-22 22:34:35 +0700

    👔 Update create document under parent document

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,16 +1,19 @@
 import { cn } from "@/lib/utils"
-import { ChevronsLeft, MenuIcon } from "lucide-react"
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, SettingsIcon } from "lucide-react"
 import { usePathname } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import { UserItem } from "./user-item"
-import { useQuery } from "convex/react"
+import { useMutation, useQuery } from "convex/react"
 import { api } from "@/convex/_generated/api"
+import { Item } from "./item"
+import { toast } from "sonner"
+import { DocumentList } from "./document-list"
 
 export const Navigation = () => {
   const pathname = usePathname()
   const isMobile = useMediaQuery("(max-width: 768px)")
-  const documents = useQuery(api.documents.get)
+  const create = useMutation(api.documents.create)
 
   const isResizingRef = useRef(false)
   const sidebarRef = useRef<ElementRef<"aside">>(null)
@@ -76,6 +79,18 @@ export const Navigation = () => {
     }
   }
 
+  const handleCreate = () => {
+    const promise = create({
+      title: "Untitled"
+    })
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created.",
+      error: "Failed to create a new note."
+    })
+  }
+
   useEffect(()=>{
     if (isMobile) {
       collapse()
@@ -115,14 +130,28 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem/>
+          <Item
+            label="Search"
+            icon={Search}
+            isSearch
+            onClick={()=>{}}
+          />
+          <Item
+            label="Setting"
+            icon={SettingsIcon}
+            onClick={()=>{}}
+          />
+          <Item 
+            onClick={handleCreate} 
+            label = "New Page" 
+            icon={PlusCircle}
+          />
         </div>
-
+       
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>
-              {document.title}
-            </p>
-          ))}
+
+          <DocumentList/>
+
         </div>
 
         <div 
